4e309446eff1891e8f60c65e8460e71c74149f47
Lajizz <ChinW66@sjtu.edu.cn>
2022-07-31 12:36:49 +0800

    fix err

--- consensus/src/leader.rs
+++ consensus/src/leader.rs
@@ -20,25 +20,25 @@ impl RRLeaderElector {
     }
 }
 
-pub struct  DummyLeaderElector {
-    committee: Committee,
-}
+// pub struct  DummyLeaderElector {
+//     committee: Committee,
+// }
 
-impl DummyLeaderElector {
-    pub fn new(committee: Committee) -> Self {
-        Self { 
-            committee,
-         }
-    }
+// impl DummyLeaderElector {
+//     pub fn new(committee: Committee) -> Self {
+//         Self { 
+//             committee,
+//          }
+//     }
 
-    pub fn get_leader(&self, round: RoundNumber) -> PublicKey {
-        let mut keys: Vec<_> = self.committee.authorities.keys().cloned().collect();
-        keys.sort();
-        keys[round as usize % self.committee.size()]
-        // for i in 0..keys.len(){
-        //     if committee.authorities[keys[i]]["id"] == 0{
-        //         break keys[i];
-        //     }
-        // }
-    }
-}
+//     pub fn get_leader(&self, round: RoundNumber) -> PublicKey {
+//         let mut keys: Vec<_> = self.committee.authorities.keys().cloned().collect();
+//         keys.sort();
+//         keys[round as usize % self.committee.size()]
+//         // for i in 0..keys.len(){
+//         //     if committee.authorities[keys[i]]["id"] == 0{
+//         //         break keys[i];
+//         //     }
+//         // }
+//     }
+// }
